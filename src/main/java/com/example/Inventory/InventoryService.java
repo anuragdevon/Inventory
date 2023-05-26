6fa747b4815a76c531a0aacafae4a17a76c989cf
anuragdevon <anuragkar@outlook.com>
2023-05-26 15:13:06 +0530

    refactor: redefine project structure

---
+++ src/main/java/com/example/Inventory/InventoryService.java
@@ -0,0 +1,4 @@
+package com.example.Inventory;
+
+public interface InventoryService {
+}
---
+++ src/main/java/com/example/Inventory/dao/ItemDao.java
@@ -0,0 +1,7 @@
+package com.example.Inventory.dao;
+
+import com.example.Inventory.model.Inventory;
+
+public class ItemDao {
+
+}
